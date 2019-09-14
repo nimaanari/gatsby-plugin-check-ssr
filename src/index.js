30aba4ff8f26f3d8c3de8dff56d2aa3778e911d0
Nima Anari <anari.nima@gmail.com>
2019-09-14 03:49:29 -0700

    InsideSSR and OutsideSSR

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
-const CheckSSR = React.createContext(false);
+export default CheckSSR = React.createContext(false);
 
-export default CheckSSR;
+export const InsideSSR = ({ children }) => <CheckSSR.Consumer>{build => (build && children)}</CheckSSR.Consumer>;
+export const OutsideSSR = ({ children }) => <CheckSSR.Consumer>{build => (!build && children)}</CheckSSR.Consumer>;
